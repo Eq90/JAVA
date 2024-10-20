d4068f1e9f997d9d9a8025318feade90294bbffd
Jean Pierre <pierreadzonyo90@gmail.com>
2024-10-20 21:08:48 +0000

    Nouvelle code

--- Principal.java
+++ Principal.java
@@ -1,5 +1,6 @@
 public class Principal {
     public static void main(String[] args) {
-      System.out.println("hello world");
+        String name = "John";
+      System.out.println(name);
       }
-  }
+  } 
